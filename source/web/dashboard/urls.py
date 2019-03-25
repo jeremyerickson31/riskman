37a5468cc57e59a92d0c9334311cf87424eb608c
Jeremy Erickson <jeremyerickson31@gmail.com>
2019-03-25 14:35:35 -0400

    adding html files with hookup

--- source/web/dashboard/urls.py
+++ source/web/dashboard/urls.py
@@ -1,4 +1,9 @@
 from django.conf.urls import url
 from . import views
 
-urlpatterns = [url(r'^$', views.index, name='index')]
+urlpatterns = [url(r'intro', views.intro, name='intro'),
+               url(r'equity', views.equity, name='equity'),
+               url(r'interest_rate_risk', views.interest_rate_risk, name='interest_rate_risk'),
+               url(r'credit_risk', views.credit_risk, name='credit_risk'),
+               url(r'one_year_transitions', views.one_year_transitions, name='one_year_transitions')
+               ]
--- source/web/dashboard/views.py
+++ source/web/dashboard/views.py
@@ -2,7 +2,37 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
-	context = {}
-	return render(request, 'dashboard/index.html', context)
-	
+
+def intro(request):
+    context = dict()
+    context["nav_tree_id"] = ["nav_tree_id_intro"]
+    context["nav_tree_parents"] = ["nav_tree_id_home"]
+    return render(request, 'dashboard/intro.html', context)
+
+
+def equity(request):
+    context = dict()
+    context["nav_tree_id"] = "nav_tree_id_equity"
+    context["nav_tree_parents"] = ["nav_tree_id_riskman"]
+    return render(request, 'dashboard/equity.html', context)
+
+
+def credit_risk(request):
+    context = dict()
+    context["nav_tree_id"] = "nav_tree_id_credit_risk"
+    context["nav_tree_parents"] = ["nav_tree_id_riskman", "nav_tree_id_fixedincome"]
+    return render(request, 'dashboard/credit_risk.html', context)
+
+
+def interest_rate_risk(request):
+    context = dict()
+    context["nav_tree_id"] = "nav_tree_id_interest_rate_risk"
+    context["nav_tree_parents"] = ["nav_tree_id_riskman", "nav_tree_id_fixedincome"]
+    return render(request, 'dashboard/interest_rate_risk.html', context)
+
+
+def one_year_transitions(request):
+    context = dict()
+    context["nav_tree_id"] = "nav_tree_id_one_year_transitions"
+    context["nav_tree_parents"] = ["nav_tree_id_tables"]
+    return render(request, 'dashboard/one_year_transitions.html', context)
