f2d9f4f246728819da6c82b48ff4d827bff79ef9
Jeremy Erickson <jeremyerickson31@gmail.com>
2019-04-03 07:16:14 -0400

    adding one year transition matrix json and config and common files

---
+++ source/web/dashboard/utils/common.py
@@ -0,0 +1,22 @@
+import json
+import config
+
+
+def get_one_year_matrix(provider):
+    """
+    function that will return the one year transition matrix of the submitted provider
+    :param provider: Credit Metrics, Moodys, S&P
+    :return: one year transition matrix
+    """
+
+    file_loc = config.model_inputs["one_year_matrix_file"]
+    f = open(file_loc)
+    data_dict = json.load(f)
+
+    try:
+        matrix = data_dict["data"][provider]["one_year_transitions"]
+    except KeyError:
+        raise Exception("Data Provider Error: Could not find data on {%s}. See docstrings Provider param for details" % provider)
+
+    return matrix
+
---
+++ source/web/dashboard/utils/config.py
@@ -0,0 +1,7 @@
+
+import os
+
+current_directory = os.getcwd()
+
+model_inputs = dict()
+model_inputs["one_year_matrix_file"] = current_directory + "\\params\\oneyeartransitions.json"
