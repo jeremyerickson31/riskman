ac46948da3e2d3b3181f3eb8b1616aabfe10407a
Jeremy Erickson <jeremyerickson31@gmail.com>
2019-04-16 15:54:12 -0400

    add get_matrix_providers

--- source/web/dashboard/utils/common.py
+++ source/web/dashboard/utils/common.py
@@ -6,6 +6,24 @@ sys.path.append(os.getcwd() + "\\dashboard\\utils")
 import config
 
 
+def get_matrix_providers():
+    """
+    will open oneyeartransitions.json and read 'providers'
+    :return: list of transition matrix providers used in oneyeartransitions.json
+    """
+
+    file_loc = config.model_inputs["one_year_matrix_file"]
+    f = open(file_loc)
+    data_dict = json.load(f)
+
+    try:
+        providers = data_dict["providers"]
+    except KeyError:
+        raise Exception("Parameter Error: Could not find 'providers' in json file")
+
+    return providers
+
+
 def get_one_year_matrix(provider):
     """
     function that will return the one year transition matrix of the submitted provider
