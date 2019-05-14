ed5bd9da8c0c9e73792d3d3ba7b10790dda77378
Jeremy Erickson <jeremyerickson31@gmail.com>
2019-05-14 07:12:17 -0400

    add folder locations for script result dumps

--- source/web/dashboard/utils/config.py
+++ source/web/dashboard/utils/config.py
@@ -8,11 +8,10 @@ current_directory = os.getcwd()
 ############################################
 model_inputs = dict()
 
-# locations of important files
+# locations of important files for calculations
 model_inputs["one_year_matrix_file"] = current_directory + "\\dashboard\\utils\\params\\oneyeartransitions.json"
-model_inputs["transition_thresholds"] = current_directory + "\\dashboard\\utils\\params\\transition_thresholds.json"
-model_inputs["pre_calc_joint_matrics"] = current_directory + "\\dashboard\\utils\\params\\pre_calc_joint_matrices.json"
-model_inputs["joint_transitions"] = current_directory + "\\dashboard\\utils\\params\\"
+model_inputs["transition_thresholds_folder"] = current_directory + "\\dashboard\\utils\\params\\transition_thresholds\\"
+model_inputs["joint_matrices_folder"] = current_directory + "\\dashboard\\utils\\params\\joint_trans_prob_matrices\\"
 
 # default assumptions for the asset return normal distribution
 model_inputs["asset_return_distribution"] = {"mu": 0.0, "sigma": 1.0}
