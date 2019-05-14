7cb4901ea72db1ae34407b05c0b6920b91de2be0
Jeremy Erickson <jeremyerickson31@gmail.com>
2019-05-14 07:12:42 -0400

    hookup jsno file saving

--- source/web/run_script.py
+++ source/web/run_script.py
@@ -5,7 +5,8 @@
 import sys
 import numpy
 from datetime import datetime
-from dashboard.utils import common, engines
+import json
+from dashboard.utils import common, engines, config
 
 
 def build_joint_trans_probs():
@@ -46,10 +47,10 @@ def build_joint_trans_probs():
 
     # load list of transition matrix providers, correlation assumptions and asset return distribution params
     common.script_logger(f, "Getting list of transition matrix providers")
-    providers_list = ["SP Ratings"]  # common.get_matrix_providers()
+    providers_list = common.get_matrix_providers()
 
     common.script_logger(f, "Getting list of correlation assumptions")
-    correlations_list = [0.15]  # common.get_correlations_list()
+    correlations_list = [0.15, 0.9]  # common.get_correlations_list()
 
     common.script_logger(f, "Getting asset return distribution parameters")
     mu, sigma = common.get_asset_return_distribution_params()
@@ -62,7 +63,7 @@ def build_joint_trans_probs():
     for provider in providers_list:
 
         # make new dictionaries to hold results of threshold and joint trans probs caluclations
-        transition_thresholds[provider] = dict()
+        transition_thresholds = dict()
         joint_matrices[provider] = dict()
 
         # calculate rating level transition thresholds, dependent only on one year transition matrix and mu and sigma
@@ -72,17 +73,25 @@ def build_joint_trans_probs():
         common.script_logger(f, "Rating Level Thresholds Complete for %s " % provider)
 
         # store provider specific rating_level_thresholds for json output
-        transition_thresholds[provider] = rating_level_thresholds
+        transition_thresholds = rating_level_thresholds
+
+        common.script_logger(f, "Storing Provider specific rating transition thresholds in json")
+        # store provider specific transition thresholds
+        thresh_filename = config.model_inputs["transition_thresholds_folder"] + str(provider) + ".json"
+        thresh_file = open(thresh_filename, "w")
+        json.dump(transition_thresholds, thresh_file)
+        thresh_file.close()
+        common.script_logger(f, "FILENAME: " + str(thresh_filename))
 
         # get rating_level_threshold keys to loop through for joint probability matrices
-        rating_labels = list(transition_thresholds[provider].keys())
+        rating_labels = list(transition_thresholds.keys())
 
         # use the rating level thresholds in numerical integration
         common.script_logger(f, "Begin Bivariate Normal Numerical Integration Between Thresholds")
         for correlation in correlations_list:
 
             # joint matrices are specific to the provider and the correlation
-            joint_matrices[provider][correlation] = dict()
+            joint_matrices[correlation] = dict()
 
             common.script_logger(f, "Calculate Joint Matrices for Provider: %s and Correlation: %s" % (provider, correlation))
 
@@ -99,34 +108,31 @@ def build_joint_trans_probs():
                     rating_pair = rating_1 + "|" + rating_2  # ie: AA|CCC, first bond AA second bond CCC
 
                     common.script_logger(f, "Get Thresholds for Rating 1: %s, Rating 2: %s" % (rating_1, rating_2))
-                    rating_1_thresholds = transition_thresholds[provider][rating_1]
-                    rating_2_thresholds = transition_thresholds[provider][rating_2]
+                    rating_1_thresholds = transition_thresholds[rating_1]
+                    rating_2_thresholds = transition_thresholds[rating_2]
 
                     common.script_logger(f, "Submitting Thresholds and Correlation Matrix for numerical integration")
                     matrix, logs = engines.threshold_numerical_integration(rating_1_thresholds, rating_2_thresholds, corr_mat)
                     common.script_logger(f, logs)
                     common.script_logger(f, "Matrix calculation complete. Next Pair of ratings")
 
-                    joint_matrices[provider][correlation][rating_pair] = matrix
+                    joint_matrices[correlation][rating_pair] = matrix
 
             common.script_logger(f, "Joint Transition Probabilities Complete for %s and %s " % (provider, correlation))
+            common.script_logger(f, "Storing Provider-Correlation specific matrices in json")
+
+            # store provider-correlation specific joint transition probability matrices
+            joint_trans_filename = config.model_inputs["joint_matrices_folder"] + str(provider) + "_" + str(correlation) + ".json"
+            joint_trans_file = open(joint_trans_filename, "w")
+            json.dump(joint_matrices, joint_trans_file)
+            common.script_logger(f, "FILENAME: " + str(joint_trans_filename))
+
             common.script_logger(f, "Next Correlation")
 
         common.script_logger(f, "All Correlations Complete. Next Provider")
 
     common.script_logger(f, "All Providers Complete.")
-    common.script_logger(f, "Storing Thresholds and Joint Transition Probabilities as json")
 
-    common.script_logger(f, "output json thresholds:")
-    common.script_logger(f, str(transition_thresholds))
-
-    common.script_logger(f, "output json joint matrices")
-    for provider in providers_list:
-        for correlation in correlations_list:
-            common.script_logger(f, str(joint_matrices[provider][correlation]))
-
-    # todo store output as json
-    # todo make a function to turn matrix from dictionary into pandas frame
     # todo add a validation so addition across the entire matrix = 1
 
 
