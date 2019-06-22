ca4746135b81b6f107ffe930cd8c427f4f72ce3a
Jeremy Erickson <jeremyerickson31@gmail.com>
2019-06-21 22:45:33 -0400

    add ajax hookup

--- source/web/dashboard/urls.py
+++ source/web/dashboard/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [url(r'intro', views.intro, name='intro'),
                url(r'interest_rate_risk', views.interest_rate_risk, name='interest_rate_risk'),
                url(r'credit_risk', views.credit_risk, name='credit_risk'),
                url(r'one_year_transitions', views.one_year_transitions, name='one_year_transitions'),
-               url(r'ajax_get_trans_matrix', views.ajax_get_trans_matrix, name='ajax_get_trans_matrix')
+               url(r'ajax_get_trans_matrix', views.ajax_get_trans_matrix, name='ajax_get_trans_matrix'),
+               url(r'ajax_cred_risk_analytics', views.ajax_cred_risk_analytics, name='ajax_cred_risk_analytics')
                ]
